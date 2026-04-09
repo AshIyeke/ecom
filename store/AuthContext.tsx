4c390441b34dbb4ee5eddd1ddc8bc20874a944f8
Ash <ashiomaiyeke8@gmail.com>
2026-04-09 09:49:12 +0100

    fixed supabase sign up redirect

--- store/AuthContext.tsx
+++ store/AuthContext.tsx
@@ -46,6 +46,9 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     const { error } = await supabase.auth.signUp({
       email,
       password,
+        options: {
+        emailRedirectTo: `${window.location.origin}/auth/confirm`,
+      },
     });
 
     if (error) return { error: error.message };
